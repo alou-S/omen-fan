17f9a4676783a28903aabcac7f4ad4f1691bf9b1
Albert Sebastian <73123610+alou-S@users.noreply.github.com>
2022-12-02 00:05:28 +0530

    Initial commit for daemon

---
+++ omen-fand.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+
+import os
+from time import sleep
+from sys import argv
+
+ec_file="/sys/kernel/debug/ec/ec0/io"
+gpu_file="/sys/class/drm/card0/device/hwmon/"+os.listdir('/sys/class/drm/card0/device/hwmon/')[0]+"/temp2_input"
+
+fan1_offset=52 #0x34
+fan2_offset=53 #0x35
+bios_offset=98 #0x62
+timer_offset=99 #0x63
+cpu_temp_offset=87 #0x57
+
+fan1_max=55
+fan2_max=57
+
+UpThreshold = [50, 60, 70, 80, 90, 95]
+DownThreshold = [48, 58, 66, 78, 86, 94]
+SpeedCurve = [20, 40, 60, 70, 85, 100]
+Ambient = 0
+Linear = 0
+PollInterval = 1
+
+def UpdateFan(speed1, speed2):
+    with open(ec_file, "r+b") as ec:
+        ec.seek(fan1_offset)
+        ec.write(bytes([speed1]))
+        ec.seek(fan2_offset)
+        ec.write(bytes([speed2]))
+
+def GetTemp():
+    with open(ec_file, "r+b") as ec:
+        ec.seek(cpu_temp_offset)
+        tempc = int.from_bytes(ec.read(1), 'big')
+    with open(gpu_file, "r") as gpu:
+        tempg = int(gpu.read())/1000
+    return max(tempc, tempg)
+
+if Linear == 0:
+    index = -1
+    oldspeed = -1
+    while True:
+        temp=GetTemp()
+        while index != 5 and temp > UpThreshold[index+1]:
+            index+=1
+        while index != -1 and temp < DownThreshold[index]:
+            index-=1
+
+        if index == -1:
+            speed=Ambient
+        else:
+            speed=SpeedCurve[index]
+        
+        if oldspeed != speed:
+            oldspeed=speed
+            UpdateFan(int(fan1_max*speed/100), int(fan2_max*speed/100))
+            os.system('clear')
+            print("Last Update")
+            print(f"Speed={speed}")
+            print(f"Temp={temp}")
+        
+        sleep(PollInterval)
