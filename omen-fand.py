b4fae9ae828f4c84ff84d3f5988acc60b83bf34c
Albert Sebastian <73123610+alou-S@users.noreply.github.com>
2023-06-11 13:55:46 +0530

    Part 2 of refactoring omen-fand.py
    - Refactored code to follow PEP naming standards
    - Removed stepping mode for fan control
    - Optimized code to use less CPU time
    - Fixed various other minor issues

--- omen-fand.py
+++ omen-fand.py
@@ -2,89 +2,102 @@
 
 import os
 import signal
+import sys
 from time import sleep
-from sys import argv
+from bisect import bisect_left
 
-ec_file="/sys/kernel/debug/ec/ec0/io"
-ipc_file="/tmp/omen-fand.PID"
+ECIO_FILE = "/sys/kernel/debug/ec/ec0/io"
+IPC_FILE = "/tmp/omen-fand.PID"
 
-fan1_offset=52 #0x34
-fan2_offset=53 #0x35
-bios_offset=98 #0x62
-timer_offset=99 #0x63
-cpu_temp_offset=87 #0x57
-gpu_temp_offset=183 #0xB7
+FAN1_OFFSET = 52  # 0x34
+FAN2_OFFSET = 53  # 0x35
+BIOS_OFFSET = 98  # 0x62
+TIMER_OFFSET = 99  # 0x63
+CPU_TEMP_OFFSET = 87  # 0x57
+GPU_TEMP_OFFSET = 183  # 0xB7
 
-fan1_max=55
-fan2_max=57
+FAN1_MAX = 55
+FAN2_MAX = 57
 
-UpThreshold = [50, 60, 70, 80, 87, 93]
-DownThreshold = [48, 58, 66, 78, 87, 93]
-SpeedCurve = [20, 40, 60, 70, 85, 100]
-Ambient = 0
-Linear = 1
-PollInterval = 1
+TEMP_CURVE = [50, 60, 70, 80, 87, 93]
+SPEED_CURVE = [20, 40, 60, 70, 85, 100]
+IDLE_SPEED = 0
+POLL_INTERVAL = 1
 
-def SigHandler(signal, frame):
+# Precaulculate slopes to reduce compute time.
+slope = []
+for i in range(1, len(TEMP_CURVE)):
+    speed_diff = SPEED_CURVE[i] - SPEED_CURVE[i - 1]
+    temp_diff = TEMP_CURVE[i] - TEMP_CURVE[i - 1]
+    slope_val = round(speed_diff / temp_diff, 2)
+    slope.append(slope_val)
+
+
+def sig_handler(signum, frame):
     os.remove("/tmp/omen-fand.PID")
-    global doLoop
-    doLoop = False
+    bios_control(True)
+    sys.exit()
 
-def UpdateFan(speed1, speed2):
-    with open(ec_file, "r+b") as ec:
-        ec.seek(fan1_offset)
-        ec.write(bytes([speed1]))
-        ec.seek(fan2_offset)
-        ec.write(bytes([speed2]))
 
-def GetTemp():
-    with open(ec_file, "rb") as ec:
-        ec.seek(cpu_temp_offset)
-        tempc = int.from_bytes(ec.read(1), 'big')
-        ec.seek(gpu_temp_offset)
-        tempg = int.from_bytes(ec.read(1), 'big')
-    return max(tempc, tempg)
+def update_fan(speed1, speed2):
+    with open(ECIO_FILE, "r+b") as ec:
+        ec.seek(FAN1_OFFSET)
+        ec.write(bytes([int(speed1)]))
+        ec.seek(FAN2_OFFSET)
+        ec.write(bytes([int(speed2)]))
 
-def BiosControl():
-    ec = open(ec_file, "r+b")
-    ec.seek(bios_offset)
-    if int.from_bytes(ec.read(1), 'big') != 6:
-        global speed
-        ec.seek(bios_offset)
-        ec.write(bytes([6]))
-        sleep(0.1)
-        ec.seek(timer_offset)
-        ec.write(bytes([0]))
-        UpdateFan(int(fan1_max*speed/100), int(fan2_max*speed/100))
 
-signal.signal(signal.SIGTERM, SigHandler)
+def get_temp():
+    with open(ECIO_FILE, "rb") as ec:
+        ec.seek(CPU_TEMP_OFFSET)
+        temp_c = int.from_bytes(ec.read(1), "big")
+        ec.seek(GPU_TEMP_OFFSET)
+        temp_g = int.from_bytes(ec.read(1), "big")
+    return max(temp_c, temp_g)
 
-with open(ipc_file, "w") as ipc:
+
+def bios_control(enabled):
+    if enabled is False:
+        with open(ECIO_FILE, "r+b") as ec:
+            ec.seek(BIOS_OFFSET)
+            ec.write(bytes([6]))
+            sleep(0.1)
+            ec.seek(TIMER_OFFSET)
+            ec.write(bytes([0]))
+    elif enabled is True:
+        with open(ECIO_FILE, "r+b") as ec:
+            ec.seek(BIOS_OFFSET)
+            ec.write(bytes([0]))
+            ec.seek(FAN1_OFFSET)
+            ec.write(bytes([0]))
+            ec.seek(FAN2_OFFSET)
+            ec.write(bytes([0]))
+
+
+signal.signal(signal.SIGTERM, sig_handler)
+
+with open(IPC_FILE, "w", encoding="utf-8") as ipc:
     ipc.write(str(os.getpid()))
 
-if Linear == 1:
-    DownThreshold = UpThreshold
-doLoop = True
-index = -1
-oldspeed = -1
+speed_old = -1
 
-while doLoop:
-    temp=GetTemp()
-    while index != 5 and temp > UpThreshold[index+1]:
-        index+=1
-    while index != -1 and temp < DownThreshold[index]:
-        index-=1
+while True:
+    temp = get_temp()
 
-    if index == -1:
-        speed=Ambient
-    elif Linear == 0 or index == 5:
-        speed=SpeedCurve[index]
+    if temp <= TEMP_CURVE[0]:
+        speed = IDLE_SPEED
+    elif temp >= TEMP_CURVE[-1]:
+        speed = SPEED_CURVE[-1]
     else:
-        speed=SpeedCurve[index]+((SpeedCurve[index+1]-SpeedCurve[index])*(1-(UpThreshold[index+1]-temp)/(UpThreshold[index+1]-UpThreshold[index])))
+        i = bisect_left(TEMP_CURVE, temp)
+        y0 = SPEED_CURVE[i - 1]
+        x0 = TEMP_CURVE[i - 1]
 
-    if oldspeed != speed:
-        oldspeed=speed
-        UpdateFan(int(fan1_max*speed/100), int(fan2_max*speed/100))
+        speed = y0 + slope[i - 1] * (temp - x0)
 
-    BiosControl()
-    sleep(PollInterval)
+    if speed_old != speed:
+        speed_old = speed
+        update_fan(FAN1_MAX * speed / 100, FAN2_MAX * speed / 100)
+
+    bios_control(False)
+    sleep(POLL_INTERVAL)
