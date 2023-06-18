20f43a4f84f63394f42c5387a66af25535783841
Albert Sebastian <73123610+alou-S@users.noreply.github.com>
2023-06-19 00:31:09 +0530

    Implement config function in omen-fan.py
    - Mainly implement config function
    - Rewrite is_valid_device() check to startup_check() that additionally creates config if necessary
    - Added device check override
    - Fixed some grammatical errors

--- omen-fan.py
+++ omen-fan.py
@@ -8,12 +8,14 @@ import glob
 from time import sleep
 
 import click
+import tomlkit
 from click_aliases import ClickAliasedGroup
 
 
 ECIO_FILE = "/sys/kernel/debug/ec/ec0/io"
 IPC_FILE = "/tmp/omen-fand.PID"
 DEVICE_FILE = "/sys/devices/virtual/dmi/id/product_name"
+CONFIG_FILE = "/etc/omen-fan/config.toml"
 BOOST_FILE = glob.glob("/sys/devices/platform/hp-wmi/hwmon/*/pwm1_enable")[0]
 FAN1_SPEED_FILE = glob.glob("/sys/devices/platform/hp-wmi/hwmon/*/fan1_input")[0]
 FAN2_SPEED_FILE = glob.glob("/sys/devices/platform/hp-wmi/hwmon/*/fan2_input")[0]
@@ -41,14 +43,55 @@ def is_root(state=0):
         return True
 
 
-def is_valid_device():
+def startup_check():
+    if not os.path.isfile(CONFIG_FILE):
+        doc = tomlkit.document()
+        doc.add(tomlkit.comment("Created by omen-fan script"))
+
+        doc.add("service", tomlkit.table())
+        doc["service"]["TEMP_CURVE"] = [50, 60, 70, 80, 87, 93]
+        doc["service"]["SPEED_CURVE"] = [20, 40, 60, 70, 85, 100]
+        doc["service"]["IDLE_SPEED"] = 0
+        doc["service"]["POLL_INTERVAL"] = 1
+
+        doc.add("script", tomlkit.table())
+        doc["script"]["BYPASS_DEVICE_CHECK"] = 0
+
+        if not is_root(1):
+            print("  WARNING: No config file present. Start as root to create.")
+            is_config = 0
+        else:
+            os.makedirs(os.path.dirname(CONFIG_FILE), exist_ok=True)
+            with open(CONFIG_FILE, "w") as file:
+                file.write(tomlkit.dumps(doc))
+            print("  INFO: Configuration file has been created")
+            is_config = 1
+    else:
+        with open(CONFIG_FILE, "r") as file:
+            doc = tomlkit.loads(file.read())
+        is_config = 1
+
     with open(DEVICE_FILE, "r") as device_file:
         device_name = device_file.read()
 
-    if any(devices not in device_name for devices in DEVICE_LIST):
-        print("  ERROR: Your laptop is not in the list of supnported laptops")
-        print("  You may manually force the app to run at your own risk")
-        sys.exit(1)
+    if (
+        any(devices not in device_name for devices in DEVICE_LIST)
+        and doc["script"]["BYPASS_DEVICE_CHECK"] != 1
+    ):
+        print("  ERROR: Your laptop is not in the list of supported laptops")
+        print("         You may manually force the app to run at your own risk")
+
+        if is_config and is_root(1):
+            choice = input("Do you want to permanently disable this check? (y/N): ")
+            if choice.lower() == "y":
+                doc["script"]["BYPASS_DEVICE_CHECK"] = 1
+                with open(CONFIG_FILE, "w") as file:
+                    file.write(tomlkit.dumps(doc))
+            else:
+                sys.exit(1)
+        else:
+            print("  Root access or config file is not present for override prompt.")
+            sys.exit(1)
 
 
 def load_ec_module():
@@ -118,11 +161,7 @@ def parse_rpm(rpm, fan, max_speed):
         sys.exit(1)
 
 
-def ni(var):
-    print("    :) Need to implement subcommand", var)
-
-
-is_valid_device()
+startup_check()
 
 
 @click.group(cls=ClickAliasedGroup)
@@ -154,8 +193,53 @@ def boost_cli(arg):
 @cli.command(
     name="configure", aliases=["config", "c"], help="Configure Fan curves for service"
 )
-def configure_cli():
-    ni("configure")
+@click.option(
+    "--temp-curve",
+    type=str,
+    help="Comma-separated list of temperature curve values",
+)
+@click.option(
+    "--speed-curve",
+    type=str,
+    help="Comma-separated list of speed curve values",
+)
+@click.option("--idle-speed", type=click.IntRange(0, 100), help="Idle fan speed value")
+@click.option("--poll-interval", type=click.FLOAT, help="Poll interval in seconds")
+@click.option("--view", is_flag=True, help="Show current config")
+def configure_cli(temp_curve, speed_curve, idle_speed, poll_interval, view):
+    is_root()
+    
+    with open(CONFIG_FILE, "r") as file:
+        doc = tomlkit.loads(file.read())
+    
+    if view:
+        print(tomlkit.dumps(doc))
+        return
+    
+    if temp_curve:
+        temp_curve = [int(x) for x in temp_curve.split(",")]
+    else:
+        temp_curve = doc["service"]["TEMP_CURVE"]
+
+    if speed_curve:
+        speed_curve = [int(x) for x in speed_curve.split(",")]
+    else:
+        speed_curve = doc["service"]["SPEED_CURVE"]
+
+
+    if idle_speed is not None:
+        doc["service"]["IDLE_SPEED"] = idle_speed
+
+    if poll_interval is not None:
+        doc["service"]["POLL_INTERVAL"] = poll_interval
+
+    if len(temp_curve) != len(speed_curve):
+        raise click.UsageError("TEMP_CURVE and SPEED_CURVE must have the same length")
+    if not all(temp_curve[i] <= temp_curve[i + 1] for i in range(len(temp_curve) - 1)):
+        raise click.UsageError("TEMP_CURVE must be in ascending order")
+
+    with open(CONFIG_FILE, "w") as file:
+        file.write(tomlkit.dumps(doc))
 
 
 @cli.command(name="service", aliases=["e"], help="Start/Stop Fan management service")
@@ -178,7 +262,7 @@ def service_cli(arg):
                 try:
                     os.kill(int(ipc.read()), signal.SIGTERM)
                 except ProcessLookupError:
-                    print(" PID file exists without process.")
+                    print(" PID file exists without a process.")
                     print(" omen-fan service was killed unexpectedly.")
                     os.remove(IPC_FILE)
                     sys.exit(1)
