0b545873a1be8ce9890144b22c44921978dc2534
Phong Pham <70873548+iamSlightlyWind@users.noreply.github.com>
2024-09-11 08:29:08 +0700

    fix daemon call
    omen-fand cannot be found without file extension or path indicator
    `FileNotFoundError: [Errno 2] No such file or directory: 'omen-fand.py'`

--- omen-fan.py
+++ omen-fan.py
@@ -253,7 +253,7 @@ def service_cli(arg):
                 print(f"  omen-fan service is already running with PID:{ipc.read()}")
         else:
             bios_control(False)
-            subprocess.Popen("omen-fand")
+            subprocess.Popen("./omen-fand.py")
             print("  omen-fan service has been started")
 
     elif arg in ["stop", "0"]:
